e24906a2cdd883682530bbfacb0570b99ea31459
TwilightPSN <jeremy_gantz@outlook.com>
2025-01-16 18:07:07 -0500

    Immersive Quests Staged | Quests Cleaned

--- kubejs/server_scripts/Mekanism/Mekanism Recipe Removals.js
+++ kubejs/server_scripts/Mekanism/Mekanism Recipe Removals.js
@@ -5,4 +5,5 @@ ServerEvents.recipes(IRRebirth => {
     IRRebirth.remove({ output: 'mekanism:ingot_tin', type: 'minecraft:smelting'})
     IRRebirth.remove({output: 'ae2:certus_quartz_dust', type: 'mekanism:crushing'})
     IRRebirth.remove({ input: '#forge:ores/draconium', type: 'mekanism:enriching'})
+    IRRebirth.remove({ input: 'create:crushed_raw_osmium'})
 })
