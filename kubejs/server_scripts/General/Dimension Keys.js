3b363991f86af2111ae316a7a0912f911743d419
TwilightRealmTV <37508557+TwilightPSN@users.noreply.github.com>
2025-01-28 13:09:47 -0500

    Custom Portal Keys

---
+++ kubejs/server_scripts/General/Dimension Keys.js
@@ -0,0 +1,68 @@
+ServerEvents.recipes(IRRebirth => {
+      IRRebirth.shaped(
+        Item.of('kubejs:end_mining_key'), 
+        [
+          'AAA',
+          'ABA',
+          'AAA'
+        ],
+        {
+          A: 'minecraft:purpur_block',
+          B: 'minecraft:shulker_shell'
+        })
+    IRRebirth.shaped(
+        Item.of('kubejs:nether_key'), 
+        [
+          'AAA',
+          'ABA',
+          'AAA'
+        ],
+        {
+          A: '#forge:ingots/steel',
+          B: 'minecraft:flint_and_steel'
+        })
+    IRRebirth.shaped(
+        Item.of('kubejs:mining_key'), 
+        [
+          'AAA',
+          'ABA',
+          'AAA'
+        ],
+        {
+          A: '#forge:plates/lead',
+          B: 'immersiveengineering:hammer'
+        })
+    IRRebirth.shaped(
+        Item.of('kubejs:nether_mining_key'), 
+        [
+          'AAA',
+          'ABA',
+          'AAA'
+        ],
+        {
+          A: '#forge:ingots/netherite',
+          B: 'minecraft:nether_star'
+        })
+    IRRebirth.shaped(
+        Item.of('kubejs:void_key'), 
+        [
+          'AAA',
+          'ABA',
+          'AAA'
+        ],
+        {
+          A: '#forge:gems/fluix',
+          B: 'ae2:fluix_pearl'
+        })
+    IRRebirth.shaped(
+        Item.of('kubejs:end_key'), 
+        [
+          'AAA',
+          'ABA',
+          'AAA'
+        ],
+        {
+          A: 'minecraft:end_stone',
+          B: 'minecraft:dragon_breath'
+        })
+})
