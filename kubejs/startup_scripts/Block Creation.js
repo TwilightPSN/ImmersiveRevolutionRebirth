eb58ef2d9050234b42e4e075cf99cd1eb25e6889
DevDyna <andrearisorti2003@gmail.com>
2025-02-08 23:23:15 +0100

    Update Block Creation.js

--- kubejs/startup_scripts/Block Creation.js
+++ kubejs/startup_scripts/Block Creation.js
@@ -5,13 +5,16 @@ StartupEvents.registry('block', IRRebirth => {
       .tagBlock('create:sequenced_assembly') 
       .tagBlock('minecraft:mineable/shovel')
       .tagBlock('forge:mineable/paxel')
-      .tagBlock('cucumber:mineable/paxel'),
-    IRRebirth.create('raw_blast_brick')
-      .displayName('Raw Blast Brick')
+      .tagBlock('cucumber:mineable/paxel')
+
+      //kubejs by default use registry id to generate display name so in some cases can be useless
+    IRRebirth.create('raw_blast_brick') 
+    // .displayName('Raw Blast Brick')
       .soundType('gravel')
       .tagBlock('minecraft:mineable/shovel')
       .tagBlock('forge:mineable/paxel')
-      .tagBlock('cucumber:mineable/paxel'),
+      .tagBlock('cucumber:mineable/paxel')
+
     IRRebirth.create('acidic_redstone_block')
       .displayName('Block of Acidic Redstone')
       .soundType('stone')
