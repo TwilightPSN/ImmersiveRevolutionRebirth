40f3388f9be312e26673c44ad8bf23b134bd6593
TwilightRealmTV <37508557+TwilightPSN@users.noreply.github.com>
2025-02-13 16:19:39 -0500

    Beta 0.13.2 [Redstone Fix]

--- kubejs/server_scripts/General/Loot Removal.js
+++ kubejs/server_scripts/General/Loot Removal.js
@@ -1,16 +1,18 @@
 LootJS.modifiers((IRRebirth) => {
   IRRebirth
     .addLootTableModifier(/^minecraft:chests\/.*/)
-    .removeLoot("minecraft:flint_and_steel")
-    .removeLoot("silentgear:helmet")
-    .removeLoot("silentgear:chestplate")
-    .removeLoot("silentgear:leggings")
-    .removeLoot("silentgear:boots");
+    .removeLoot(["minecraft:flint_and_steel",
+      "silentgear:helmet",
+      "silentgear:chestplate",
+      "silentgear:leggings",
+      "silentgear:boots"
+    ]);
   IRRebirth
     .addLootTableModifier(/^minecraft:barrel\/.*/)
-    .removeLoot("minecraft:flint_and_steel")
-    .removeLoot("silentgear:helmet")
-    .removeLoot("silentgear:chestplate")
-    .removeLoot("silentgear:leggings")
-    .removeLoot("silentgear:boots");
+    .removeLoot(["minecraft:flint_and_steel",
+      "silentgear:helmet",
+      "silentgear:chestplate",
+      "silentgear:leggings",
+      "silentgear:boots"
+    ]);
 });
